904998eeb66a2941edac637bc3bc55c33187fe1a
Arghyadeep Das <arghyadeep1899@gmail.com>
2023-10-12 03:47:21 +0530

    Fixed MS SQL connection

--- db/database.py
+++ db/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
@@ -15,16 +15,12 @@ DB_NAME = os.getenv("DB_NAME")
 from sqlalchemy import URL
 
 url_object = URL.create(
-    "mysql+mysqlconnector",
+    "mssql+pymssql",
     username=DB_USER,
     password=DB_PASSWORD,  # plain (unescaped) text
     host=DB_HOST,
     database=DB_NAME,
-    # ssl_ca="C:\\Users\\Dell\\Downloads\\DigiCertGlobalRootG2.crt.pem"
 )
-
-#SQLALCHEMY_DATABASE_URL = f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(url_object, echo=True)
 print(engine)
