5e54092a869e2049595a5d353897a729983a99c4
Shreeya Thigale <shriyathigale20@gmail.com>
2023-10-12 02:15:11 +0530

    adding yml

--- src/app/user-journey/user-journey.component.ts
+++ src/app/user-journey/user-journey.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 
+
 @Component({
   selector: 'user-journey',
   templateUrl: './user-journey.component.html',
@@ -8,6 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserJourneyComponent implements OnInit {
 
+  active = [
+    {
+      id: 1,
+      title: 'Appointment 1',
+      dateTime: '2023-10-12T01:15:00',
+      therapist: 'Arghyadeep',
+      feedback: 'Feedback',
+      status: 'Active',
+    },
+  ]
+
+  upcoming = [
+    {
+      id: 1,
+      title: 'Appointment 1',
+      dateTime: '2023-10-12T01:15:00',
+      therapist: 'Arghyadeep',
+      feedback: 'Feedback',
+      status: 'Upcoming',
+    },
+    {
+      id: 2,
+      title: 'Appointment 2',
+      dateTime: '2023-10-20T11:30:00',
+      therapist: 'Arghyadeep',
+      feedback: 'Feedback',
+      status: 'Upcoming',
+    },
+    {
+      id: 2,
+      title: 'Appointment 0',
+      dateTime: '2023-10-10T11:30:00',
+      therapist: 'Arghyadeep',
+      feedback: 'Feedback',
+      status: 'Upcoming',
+    },
+
+    {
+      id: 2,
+      title: 'Appointment 0',
+      dateTime: '2023-10-10T11:30:00',
+      therapist: 'Arghyadeep',
+      feedback: 'Feedback',
+      status: 'Upcoming',
+    },
+  ];
+
+  completed = [
+    {
+      id: 2,
+      title: 'Appointment 0',
+      dateTime: '2023-10-10T11:30:00',
+      therapist: 'Arghyadeep',
+      feedback: 'Feedback',
+      status: 'Completed',
+    },
+  ];
   userJourney = 1
   constructor() { }
 
