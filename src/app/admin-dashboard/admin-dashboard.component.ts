f594f86895b2e7ce879b41da38f57b57a8a6b5cc
Shreeya Thigale <shriyathigale20@gmail.com>
2023-10-12 14:43:48 +0530

    Adding dashboard

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  referred: any
+  closed: any
+  funds: any
+  counsellor: any
+  highrisk: any
+  constructor(private http: HttpClient) { }
+
+  header = new HttpHeaders({
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Access-Control-Allow-Origin": "https://whizbang-codefest-api.azurewebsites.net",
+    "Access-Control-Allow-Headers": "*"
+  })
 
   ngOnInit(): void {
+    this.http.get("https://whizbang-codefest-api.azurewebsites.net/cases/referred", { headers: this.header }).subscribe(data => {
+      if (data != undefined) {
+        this.referred = data
+        console.log(this.referred)
+
+      }
+    })
+
+    this.http.get("https://whizbang-codefest-api.azurewebsites.net/cases/closed", { headers: this.header }).subscribe(data => {
+      if (data != undefined) {
+        this.closed = data
+        console.log(this.closed)
+
+      }
+    })
+
+    this.http.get("https://whizbang-codefest-api.azurewebsites.net/cases/funds", { headers: this.header }).subscribe(data => {
+      if (data != undefined) {
+        this.funds = data
+        console.log(this.funds)
+
+      }
+    })
+
+    this.http.get("https://whizbang-codefest-api.azurewebsites.net/cases/counsellor", { headers: this.header }).subscribe(data => {
+      if (data != undefined) {
+        this.counsellor = data
+        console.log(this.counsellor)
+
+      }
+    })
+
+    this.http.get("https://whizbang-codefest-api.azurewebsites.net/cases/highrisk", { headers: this.header }).subscribe(data => {
+      if (data != undefined) {
+        this.highrisk = data
+        console.log(this.highrisk)
+
+      }
+    })
   }
 
 }
